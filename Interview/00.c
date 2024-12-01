a5167c7e6e1b4c97216680f77b438c1f99822cb3
Nixon T X <nixontx321@gmail.com>
2024-12-01 14:54:19 +0530

    found what, where went wrong. practicing problems

--- Interview/00.c
+++ Interview/00.c
@@ -1,52 +1,43 @@
-#include <stdio.h>
+#include<stdio.h>
 
-// Function to move zeros to the end of the array
 void moveZerosToEnd(int arr[], int n) {
-    int count = 0; // Count of non-zero elements
+    int count = 0;
 
-    // Traverse the array. If an element is non-zero, place it at the 'count' index
-    for (int i = 0; i < n; i++) {
-        if (arr[i] != 0) {
+    for(int i=0; i<n; i++) {
+        if(arr[i] != 0) {
             arr[count++] = arr[i];
         }
     }
 
-    // Fill the remaining positions with zeros
     while (count < n) {
         arr[count++] = 0;
     }
-}
-
-// Function to print the array
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
+    
 }
 
 int main() {
-    int n;
+    int n, i;
 
-    printf("Enter the number of elements in the array: ");
+    printf("enter the no. of ele in the Array: ");
     scanf("%d", &n);
-
+    
     int arr[n];
-
-    printf("Enter the elements of the array: ");
-    for (int i = 0; i < n; i++) {
+    printf("Enter the ele: ");
+    for(i= 0; i<n; i++) {
         scanf("%d", &arr[i]);
     }
 
-    printf("The Previous Array: \n");
-    for (int i = 0; i < n; i++) {
-        printf("%d", arr[i]);
+    printf("Array Before: ");
+    for(i= 0; i<n; i++) {
+        printf("%d ", arr[i]);
     }
 
     moveZerosToEnd(arr, n);
 
-    printf("Array after moving zeros to the end: ");
-    printArray(arr, n);
+    printf("\nArray After: ");
+    for(i = 0; i<n; i++) {
+        printf("%d ", arr[i]);
+    }
 
     return 0;
 }
--- Interview/practice.c
+++ Interview/practice.c
@@ -1,7 +1,43 @@
+#include<stdio.h>
 
-// int i = 0;
+void moveZerosToEnd(int arr[], int n) {
+    int count = 0;
 
-// while(arr[n-i] != '0') {
-//     arr[i] = arr[i+1]
-//     i++;
-// }
+    for(int i=0; i<n; i++) {
+        if(arr[i] != 0) {
+            arr[count++] = arr[i];
+        }
+    }
+
+    while (count < n) {
+        arr[count++] = 0;
+    }
+    
+}
+
+int main() {
+    int n, i;
+
+    printf("enter the no. of ele in the Array: ");
+    scanf("%d", &n);
+    
+    int arr[n];
+    printf("Enter the ele: ");
+    for(i= 0; i<n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Array Before: ");
+    for(i= 0; i<n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    moveZerosToEnd(arr, n);
+
+    printf("\nArray After: ");
+    for(i = 0; i<n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}

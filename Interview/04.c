b1ce264c358e51275915fb26a2ec3afa8c6f903f
Nixon T X <nixontx321@gmail.com>
2025-07-10 10:15:23 +0530

    two-pointer technique, from a hackathon

--- Interview/04.c
+++ Interview/04.c
@@ -1,29 +1,42 @@
 // Given two sorted arrays, merge them into a single sorted array without using a sorting function
-
 #include<stdio.h>
-int main() {
-    int arr1 = {1, 2, 3, 4, 5};
-    int arr2 = {4, 5, 6, 7, 8};
 
+void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2, int merged[]) {
+    int i=0, j=0, k=0;
+
+    while(i < size1 && j < size2) {
+        if(arr1[i] < arr2[j]) {
+            merged[k++] = arr1[i++];
+            // i++;
+        } else {
+            merged[k++] = arr2[j++];
+        }
+    }
+
+    while(i < size1) {
+        merged[k++] = arr1[i++];
+    }
+
+    while(j < size2) {
+        merged[k++] = arr2[j++];
+    }
+
+}
+
+int main() {
+    int arr1[] = {1, 2, 3, 4, 5};
+    int size1 = sizeof(arr1) / sizeof(arr1[0]);
+
+    int arr2[] = {4, 5, 6, 7, 8};
+    int size2 = sizeof(arr2) / sizeof(arr2[0]);
+
+    int merged[size1 + size2];
+    mergeSortedArrays(arr1, size1, arr2, size2, merged);
     
-
-
-    return 0;
-}
-
-
-/*
-
-#include<stdio.h>
-int main() {
-    int a = 10;
-    int *p = &a;
-    int **q = &p;
-    int ***r=&q;
-
-    printf("*p=%d a=%d **q=%d ***r=%d",*p, a, **q, ***r);
+    printf("Merged array: \n");
+    for(int i=0; i < size1+size2; i++) {
+        printf("%d ", merged[i]);
+    }
 
     return 0;
 }
-
-*/
